a78f50395c3437b2f9926fed985028cbf521a9e4
w31loa <alexslava.barannik@gmail.com>
2024-02-26 02:25:12 +0500

    UPD: prisma schema, ADD: user model

--- src/user/model/user.model.ts
+++ src/user/model/user.model.ts
@@ -1,7 +1,36 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
 
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(()=> ID , {nullable:false})
+  id!: number
+
+  @Field(()=> String, {nullable: false})
+  login!: string
+
+  @Field(()=> String, {nullable: false})
+  password!: string
+
+  @Field(()=> String, {nullable: false})
+  name!: string
+
+  @Field(()=> String, {nullable: false})
+  surname!: string
+
+  @Field(()=> Date , {nullable: false})
+  dob!: Date
+
+  @Field(()=> String, {nullable: false})
+  photoPath!: string
+
+  @Field(()=> Role, {nullable: false, defaultValue: "USER"})
+  role!: Role
+
+}
+
+
+
+enum Role{
+  USER,
+  ADMIN
 }
